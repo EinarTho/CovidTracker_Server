512feaaddd32c3b7ca0e4b418eba090265150f76
Einar Thomasen <e_thomasen@hotmail.com>
2020-11-20 22:07:21 +0100

    some routes and queriesworking

--- model/mongooseModel.js
+++ model/mongooseModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const { Schema } = mongoose;
 
 const connect = (password, dbName) => {
   const dbUrl = `mongodb+srv://Einar:${password}@cluster0.8zyic.mongodb.net/${dbName}?retryWrites=true&w=majority`;
@@ -16,4 +17,26 @@ const connect = (password, dbName) => {
   });
 };
 
-module.exports.connect = connect;
+//rooms visited will be an array of objects containing the room + time and date?
+
+const employee = new Schema({
+  id: { type: Number, required: true },
+  name: { type: String, required: true },
+  roomsVisited: Array,
+});
+
+//visiters will be and object per day, stored in an array,
+const room = new Schema({
+  id: { type: Number, require: true },
+  name: { type: String, required: true }, //add an array here for custom message?
+  visiters: Array,
+});
+
+const employeeModel = mongoose.model('Employee', employee);
+const roomModel = mongoose.model('Room', room);
+
+module.exports = {
+  connect,
+  employeeModel,
+  roomModel,
+};
